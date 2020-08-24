3194e6f9b73b65180c7df79f5b7130743632aaff
Thomas Artmann <artmann@die-etagen.de>
2020-08-24 16:46:52 +0200

    Implemented SuccessfulResponseTest.php

---
+++ tests/Unit/SuccessfulResponseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Appletie\Response\Json\ApiResponse;
+use Appletie\Response\Json\SuccessfulResponse;
+
+test('class SuccessfulResponse exists', function () {
+    $this->assertTrue(class_exists("Appletie\Response\Json\SuccessfulResponse"));
+});
+
+it('can create an SuccessfulResponse', function () {
+    $successfulResponse = new SuccessfulResponse();
+    $this->assertNotNull($successfulResponse);
+});
+
+it('is an instance of ApiResponse', function () {
+    $successfulResponse = new SuccessfulResponse();
+    $this->assertTrue($successfulResponse instanceof ApiResponse);
+});
+
+it('has status "OK"', function () {
+    $successfulResponse = new SuccessfulResponse();
+    $content = $successfulResponse->getContentArray();
+    $this->assertTrue(isset($content['status']));
+    $this->assertEquals('OK', $content['status']);
+});
+
+it('has data', function () {
+    $successfulResponse = new SuccessfulResponse(DEFAULT_EXTRA_DATA);
+    $content = $successfulResponse->getContentArray();
+    $this->assertTrue(isset($content['data']));
+    $this->assertEquals($content['data'], DEFAULT_EXTRA_DATA);
+});
