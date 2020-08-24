aef5ca4014d4c19bba036528be59922d73de2524
Thomas Artmann <artmann@die-etagen.de>
2020-08-24 16:28:25 +0200

    Added SuccessfulResponse.php

---
+++ src/Json/SuccessfulResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Appletie\Response\Json;
+
+class SuccessfulResponse extends ApiResponse
+{
+    /**
+     * SuccessfulResponse constructor.
+     * @param array|null $extra
+     * @param int $status
+     * @param array $headers
+     */
+    public function __construct(array $extra = null, int $status = 200, array $headers = [])
+    {
+        $data = [];
+
+        if ($extra) {
+            $data['data'] = $extra;
+            $data['checksum'] = md5(json_encode($extra));
+        }
+
+        parent::__construct(ApiResponse::RESPONSE_STATUS_OK, $data, $status, $headers);
+    }
+}
