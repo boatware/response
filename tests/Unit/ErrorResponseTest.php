01ee95154d62c996cdbd4abf31151f6ec3bf12ea
Thomas Artmann <artmann@die-etagen.de>
2020-08-24 16:28:09 +0200

    Added ErrorResponseTest.php

---
+++ tests/Unit/ErrorResponseTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Appletie\Response\Json\ApiResponse;
+use Appletie\Response\Json\ErrorResponse;
+
+test('class ErrorResponse exists', function () {
+    $this->assertTrue(class_exists("Appletie\Response\Json\ErrorResponse"));
+});
+
+it('can create an ErrorResponse', function () {
+    $errorResponse = new ErrorResponse(DEFAULT_ERROR_MESSAGE);
+    $this->assertNotNull($errorResponse);
+});
+
+it('is an instance of ApiResponse', function () {
+    $errorResponse = new ErrorResponse(DEFAULT_ERROR_MESSAGE);
+    $this->assertTrue($errorResponse instanceof ApiResponse);
+});
+
+it('has status "error"', function () {
+    $errorResponse = new ErrorResponse(DEFAULT_ERROR_MESSAGE);
+    $content = $errorResponse->getContentArray();
+    $this->assertTrue(isset($content['status']));
+    $this->assertEquals($content['status'], 'error');
+});
+
+it('has an error message', function () {
+    $errorResponse = new ErrorResponse(DEFAULT_ERROR_MESSAGE);
+    $content = $errorResponse->getContentArray();
+    $this->assertTrue(isset($content['message']));
+    $this->assertEquals($content['message'], DEFAULT_ERROR_MESSAGE);
+});
+
+it('has extra data', function () {
+    $errorResponse = new ErrorResponse(DEFAULT_ERROR_MESSAGE, DEFAULT_ERROR_EXTRA_DATA);
+    $content = $errorResponse->getContentArray();
+    $this->assertTrue(isset($content['extra']));
+    $this->assertEquals($content['extra'], DEFAULT_ERROR_EXTRA_DATA);
+});
